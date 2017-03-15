0134749cd1b350c579e10f0bdf6662b7875ee18c
steijnb <steijnbakker@gmail.com>
2017-03-15 16:38:36 +0100

    Update on Index
    Learning on how to fill the index.php file and how to commit this to
    GitHub Master

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>learnGitHub</title>
+</head>
+<body>
+    <h1>Learn GitHub!</h1>
+    <p>Step by step I learn how to use GitHub.</p>
+</body>
+</html>
